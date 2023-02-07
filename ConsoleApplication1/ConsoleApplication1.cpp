d5ecd760f9d3a64296be9dea23104c064f514cf4
RinaBoni <brisovae@mail.ru>
2023-02-07 13:50:47 +0900

    с компа

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,55 @@
+﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <time.h>
+using namespace std;
+
+int** create_memory_matrix(int n)//выделение памяти под матрицу
+{
+    int** matrix = new int* [n];//выделяем память под массив указателей
+    for (int i = 0; i < n; i++)
+    {
+        matrix[i] = new int[n];
+    }
+    return matrix;
+}
+
+void filling_matrix(int** matrix, int n)//заполнение матрицы
+{
+    srand(time(NULL));
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i][j] = rand() % 10 - 1;
+            cout << matrix[i][j] << "   ";
+        }
+        cout << "\n";
+    }
+   // return matrix;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int** MatrixA = create_memory_matrix(n);
+    filling_matrix(MatrixA, n);
+
+  //  int** MatrixA = new int* [n];//выделение под указатели
+  //  int* mas = new int [n];
+   /* for (int i = 0; i < n; i++)
+    {
+        MatrixA[i] = new int[n];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            MatrixA[i][j] = rand() % 10 - 1;
+            cout << MatrixA[i][j] << "   ";
+        }
+        cout << "\n";
+    }*/
+}
---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <time.h>
+
+int main()
+{
+    POINT op;
+    HWND hwnd = GetConsoleWindow();
+    HDC pero = GetDC(hwnd);
+    SelectObject(pero, GetStockObject(WHITE_PEN));
+
+
+
+    MoveToEx(pero, 100, 100, &op);
+    LineTo(pero, 100, 175);
+    for (int i = 25; i <= 100; i++) {
+        MoveToEx(pero, 100, 175, &op);
+        LineTo(pero, i, 100);
+    }
+
+    MoveToEx(pero, 100, 100, &op);
+    LineTo(pero, 0, 200);
+
+    //по y
+    for (int i = 185; i <= 200; i++) {
+        MoveToEx(pero, 85, 100, &op);
+        LineTo(pero, 0, i);
+    }
+
+    //по x
+    for (int i = 0; i <= 15; i++) {
+        MoveToEx(pero, 100, 115, &op);
+        LineTo(pero, i, 200);
+    }
+
+    //по y
+    for (int i = 185; i <= 200; i++) {
+        MoveToEx(pero, 100, 100, &op);
+        LineTo(pero, 0, i);
+    }
+
+    //по x
+    for (int i = 0; i <= 15; i++) {
+        MoveToEx(pero, 100, 100, &op);
+        LineTo(pero, i, 200);
+    }
+
+
+
+    /*//прикольный логотип
+    MoveToEx(pero, 100, 100, &op);
+    LineTo(pero, 100, 175);
+    for (int i = 25; i <= 100; i++) {
+        MoveToEx(pero, 100, 175, &op);
+        LineTo(pero, i, 100);
+    }
+
+    MoveToEx(pero, 100, 100, &op);
+    LineTo(pero, 0, 200);*/
+
+    /*по y
+    for (int i = 185; i <= 200; i++) {
+        MoveToEx(pero, 100, i, &op);
+        LineTo(pero, 0, i);
+    }
+
+    //по x
+    for (int i = 0; i <= 15; i++) {
+        MoveToEx(pero, i, 100, &op);
+        LineTo(pero, i, 200);
+    }//*/
+
+
+
+    ReleaseDC(hwnd, pero);
+    std::cin.get();
+}
