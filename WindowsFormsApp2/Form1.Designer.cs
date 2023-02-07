64f489dfab32f3affbd2efc9a71efbc080aa40eb
RinaBoni <brisovae@mail.ru>
2023-02-07 13:54:53 +0900

    с компа

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.calc = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.enter_a = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // calc
+            // 
+            this.calc.Location = new System.Drawing.Point(431, 148);
+            this.calc.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.calc.Name = "calc";
+            this.calc.Size = new System.Drawing.Size(284, 46);
+            this.calc.TabIndex = 0;
+            this.calc.Text = "Вычислить";
+            this.calc.UseVisualStyleBackColor = true;
+            this.calc.Click += new System.EventHandler(this.calc_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Location = new System.Drawing.Point(22, 37);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(706, 72);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Дано действительное число а. Вычислить f(a), где f—периодическая функция\r\n с пери" +
+    "одом 2, совпадающая на отрезке [—1, 1]:\r\nа) с функцией — x^2 + 1;";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.Location = new System.Drawing.Point(22, 156);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Введите а:";
+            // 
+            // enter_a
+            // 
+            this.enter_a.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.enter_a.Location = new System.Drawing.Point(152, 156);
+            this.enter_a.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.enter_a.Name = "enter_a";
+            this.enter_a.Size = new System.Drawing.Size(162, 29);
+            this.enter_a.TabIndex = 3;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 24;
+            this.listBox1.Location = new System.Drawing.Point(26, 231);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(699, 220);
+            this.listBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(767, 494);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.enter_a);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.calc);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Задача 72а";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button calc;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox enter_a;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calc_Click(object sender, EventArgs e)
+        {
+            string str = enter_a.Text;
+            float a = Convert.ToSingle(str);
+
+            while (a < -1)
+            {
+                a += 2;
+            }
+            while (a > 1)
+            {
+                a -= 2;
+            }
+            listBox1.Items.Add(-(a * a) + 1);
+        }
+    }
+}
